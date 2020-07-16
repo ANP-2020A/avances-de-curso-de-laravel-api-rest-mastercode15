406edb22687721df6c1097f856f90074e1fbb090
Sebastian <sebastian.morales@epn.edu.ec>
2020-07-15 23:14:16 -0500

    #23 Validacion de datos CommentController ValidaciÃon de categoria de articulo Validación de titulo unico con excepcion de un update

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,18 +1,14 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Article;
 use App\Http\Resources\Article as ArticleResource;
 use App\Http\Resources\ArticleCollection;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
 {
-    private static $rules = [
-        'title' => 'required|string|unique:articles|max:255',
-        'body' => 'required',
-    ];
-
     private static $messages = [
         'required' => 'El campo :attribute es obligatorio.',
         'body.required' => 'El body no es válido.',
@@ -22,6 +18,7 @@ class ArticleController extends Controller
     {
         return new ArticleCollection(Article::paginate(10));
     }
+
     public function show(Article $article)
     {
         return response()->json(new ArticleResource($article), 200);
@@ -29,24 +26,28 @@ class ArticleController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate(self::$rules, self::$messages);
-
-//        $validator = Validator::make($request->all(), [
-//            'title' => 'required|string|unique:articles|max:255',
-//            'body' => 'required|string'
-//        ]);
-//        if ($validator->fails()) {
-//            return response()->json(['error' => 'data_validation_failed', "error_list"=>$validator->errors()], 400);
-//        }
+        $request->validate([
+            'title' => 'required|string|unique:articles|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'
+        ], self::$messages);
 
         $article = Article::create($request->all());
         return response()->json($article, 201);
     }
+
     public function update(Request $request, Article $article)
     {
+        $request->validate([
+            'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'
+        ], self::$messages);
+
         $article->update($request->all());
         return response()->json($article, 200);
     }
+
     public function delete(Request $request, Article $article)
     {
         $article->delete();
